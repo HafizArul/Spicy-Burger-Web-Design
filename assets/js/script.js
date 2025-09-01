bfbd32bb1a08e91c8050e492e15ca510891c6491
Hafiz Arul <hafizarul1604@outlook.co.id>
2025-09-01 16:37:11 +0700

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const navElement = document.querySelector("nav.navbar");
+const heroSection = document.querySelector("header.hero");
+
+window.addEventListener("resize", () => {
+    const navAutoMargin = navElement.clientHeight;
+    heroSection.style.setProperty('--header-auto-margin', `calc(${navAutoMargin}px + 12px)`);
+    console.log(navAutoMargin);
+});
