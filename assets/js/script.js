3398ae2aeedef5b86b8282ec9262609d4bc10677
Hafiz Arul <hafizarul1604@outlook.co.id>
2025-09-29 06:55:16 +0700

    Menghapus console.log pada script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,4 @@ const heroSection = document.querySelector("header.hero");
 window.addEventListener("resize", () => {
     const navAutoMargin = navElement.clientHeight;
     heroSection.style.setProperty('--header-auto-margin', `calc(${navAutoMargin}px + 12px)`);
-    console.log(navAutoMargin);
 });
